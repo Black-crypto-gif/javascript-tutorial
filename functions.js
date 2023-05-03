9f2a711ebacebae8258e007f3fa64c8a5483618b
MELLAL Fethi <80727888+Black-crypto-gif@users.noreply.github.com>
2023-05-03 09:06:42 +0100

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,12 @@
+function sayHello(name) {
+  console.log("Hello, " + name + "!");
+}
+
+sayHello("John"); // output: "Hello, John!"
+
+// Arrow Functions :
+
+let add = (x, y) => x + y;
+
+let result = add(2, 3); // assigns 5 to result
+console.log(result);   // logs 5 
