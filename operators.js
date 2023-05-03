5a69dc8f2ac3bec86a2367051b085255115701e5
MELLAL Fethi <80727888+Black-crypto-gif@users.noreply.github.com>
2023-05-03 08:52:44 +0100

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,46 @@
+// Arithmetic operators
+let x = 10;
+let y = 5;
+
+console.log(x + y); // 15
+console.log(x - y); // 5
+console.log(x * y); // 50
+console.log(x / y); // 2
+console.log(x % y); // 0
+console.log(x ** y); // 100000
+
+
+
+// Assignment operators
+let z = 5;
+z += 2; // equivalent to z = z + 2
+console.log(z); // 7
+
+z -= 2; // equivalent to z = z - 2
+console.log(z); // 5
+
+z *= 2; // equivalent to z = z * 2
+console.log(z); // 10
+
+z /= 2; // equivalent to z = z / 2
+console.log(z); // 5
+
+z %= 2; // equivalent to z = z % 2
+console.log(z); // 1
+
+
+
+// Comparison operators
+console.log(x > y);  // true
+console.log(x < y);  // false
+console.log(x >= y); // true
+console.log(x <= y); // false
+console.log(x === y); // false
+console.log(x !== y); // true
+
+
+
+// Logical operators
+console.log(x > 5 && y < 10); // true
+console.log(x > 5 || y > 10); // true
+console.log(!(x > 5));       // false
