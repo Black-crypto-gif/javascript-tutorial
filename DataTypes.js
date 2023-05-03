4f6086364122f66b530cd410c6e73b25c0c029aa
MELLAL Fethi <80727888+Black-crypto-gif@users.noreply.github.com>
2023-05-03 08:45:45 +0100

    Create DataTypes.js

---
+++ DataTypes.js
@@ -0,0 +1,31 @@
+// Number
+let age = 28;
+let price = 9.99;
+
+// String
+let name = "John";
+let message = "Hello, world!";
+
+// Boolean
+let isOldEnough = true;
+let hasDiscount = false;
+
+// Null
+let discount = null;
+
+// Undefined
+let address;
+let phoneNumber = undefined;
+
+// Symbol
+let id1 = Symbol();
+let id2 = Symbol("id");
+
+// Object
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 28
+};
+
+let colors = ["red", "green", "blue"];
