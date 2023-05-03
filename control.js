824a446a31f5694026ef98a6bc97f3c406eb2e29
MELLAL Fethi <80727888+Black-crypto-gif@users.noreply.github.com>
2023-05-03 08:58:50 +0100

    Create control.js

---
+++ control.js
@@ -0,0 +1,43 @@
+//if/else
+let age = 25;
+
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a minor");
+} 
+
+// switch case :
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+  case 3:
+    console.log("Today is Wednesday");
+    break;
+  default:
+    console.log("Today is not a weekday");
+} 
+// for / while do loop :
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+} 
+
+let i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+} 
+
+let i = 1;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 5); 
