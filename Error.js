65dd8bedaffb353a1e9cd3b1f7866b1df8459b8a
MELLAL Fethi <80727888+Black-crypto-gif@users.noreply.github.com>
2023-05-03 10:43:33 +0100

    Create Error.js

---
+++ Error.js
@@ -0,0 +1,8 @@
+try {
+  // Code that may throw an error
+  const result = doSomething();
+  console.log(result);
+} catch (error) {
+  // Code to handle the error
+  console.error('An error occurred:', error.message);
+}
